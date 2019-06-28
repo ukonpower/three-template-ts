2ff592e309d8fe89907a486893ad8078476c30fd
ukonpower <ukon.ryohei@junni.jp>
2019-06-28 17:09:01 +0900

    clean up

--- src/ts/MainScene.ts
+++ src/ts/MainScene.ts
@@ -21,31 +21,55 @@ export default class MainScene extends ORE.BaseScene{
 
 		this.renderer = this.gProps.renderer;
 		
-		this.camera.position.set(0,1.5,3);
-        this.camera.lookAt(0,0,0);
+		this.camera.position.set( 0, 1.5, 3 );
+        this.camera.lookAt( 0, 0, 0 );
 
-        var boxGeo = new THREE.BoxGeometry(1,1,1);
+        var boxGeo = new THREE.BoxGeometry( 1, 1, 1 );
         var boXMat = new THREE.MeshNormalMaterial();
-        this.box = new THREE.Mesh(boxGeo,boXMat);
-		this.scene.add(this.box);
+        this.box = new THREE.Mesh( boxGeo, boXMat );
+		this.scene.add( this.box );
 
         this.light = new THREE.DirectionalLight();
         this.light.position.y = 10;
-		this.scene.add(this.light);
+		this.scene.add( this.light );
+
 	}
 
-	animate(){
+	public onUnbind( ){
+
+	}
+
+	public animate(){
+
         this.box.rotateY(0.01);
+		
 		this.renderer.render(this.scene,this.camera);
+	
 	}
 
-	onResize(width, height) {
+	public onResize(width, height) {
+		
 		super.onResize(width,height);
+	
 	}
 
-    onTouchStart(cursor) { }
+    public onTouchStart( cursor: ORE.Cursor, event: MouseEvent ) {
 
-    onTouchMove(cursor) { }
+	}
 
-    onTouchEnd(cursor) { }
+    public onTouchMove( cursor: ORE.Cursor, event: MouseEvent ) {
+
+	}
+
+    public onTouchEnd( cursor: ORE.Cursor, event: MouseEvent ) {
+
+	}
+
+	public onHover( cursor: ORE.Cursor ){
+
+	}
+
+	public onWheel( event: WheelEvent ){
+
+	}
 }
